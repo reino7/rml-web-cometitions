92826f46648846930f91ed5865b508510c5bcb16
Reino Ristissaar <reino@ristissaar.ee>
2022-03-22 14:06:23 +0200

    registration ui add/remove from reg players table

--- public/js/competition-register-script.js
+++ public/js/competition-register-script.js
@@ -74,15 +74,17 @@ competitionName.innerText = localStorage.getItem('compName')
 
 /* get reiting and reigster table html elements */ 
 const reitingsTableFull = document.getElementById('reitingsTableFull');
+const registerTableFull = document.getElementById('registerTableFull');
 const registerTableBody = document.getElementById('registerTableBody');
 
 /* display the reitings players count */
 const reitingsPlayerCount = document.getElementById('reitingPlayerCount');
 reitingsPlayerCount.innerHTML = reitingsTableFull.rows.length - 1;
 
+
 let registerTableData = []
 let registerTablePersonId = []
-
+let rowIndexValue;
 
 function find(n){
   let found = "";
@@ -91,10 +93,59 @@ function find(n){
   return found;
 }
 
+function countRegisteredPlayers(){
+  const registeredPlayerCount = document.getElementById(
+    'registeredPlayerCount'
+  );
+  for (let i = 0; i < registerTableFull.rows.length; i++) {
+    return registeredPlayerCount.innerHTML = registerTableFull.rows.length;
+  }
+}
+
+function deleteRow() {
+  for (let i = 0; i < registerTableFull.rows.length; i++) {
+    registerTableFull.rows[i].addEventListener('click', function() {
+      rowIndexValue = registerTableFull.rows[i].rowIndex;
+      console.log(rowIndexValue)
+      registerTableFull.deleteRow(rowIndexValue)
+      registerTableData.splice(rowIndexValue-1, 1)
+      reloadRows();
+      console.table(registerTableData);
+
+    });
+  }
+}
+
+function reloadRows() {
+  registerTableBody.innerHTML = '';
+
+  for (let i = 0; i < registerTableData.length; i++) {
+    registerTableBody.innerHTML += `
+      <tr id="reg${registerTableData[i].personId}" class="">
+        <td class="text-center">${i + 1}</td>
+        <td class="text-center">${registerTableData[i].rateOrder}</td>
+        <td class="text-center">${registerTableData[i].ratePoints}</td>
+        <td class="text-center">${registerTableData[i].personId}</td>
+        <td>${registerTableData[i].firstLastName}</td>
+        <td class="text-center">${registerTableData[i].birthdate}</td>
+        <td class="text-center">${registerTableData[i].sex}</td>
+        <td class="text-center">
+          <a class="text-danger" onclick="deleteRow()">
+            <i class="fas fa-trash-alt"></i>
+          </a>
+        </td>
+      </tr>
+    `;
+  }
+
+}
+
 // add EventListener to every row to reitingsTableFull
 // i = 1 skip table header
 for (let i = 1; i < reitingsTableFull.rows.length; i++) {
   reitingsTableFull.rows[i].addEventListener('click', function () {
+    /* display registered player count */
+    countRegisteredPlayers();
 
     // console.log(this)
     registerTableData.push({
@@ -105,54 +156,29 @@ for (let i = 1; i < reitingsTableFull.rows.length; i++) {
       birthdate: this.cells[4].innerText,
       sex: this.cells[5].innerText,
     });
-    registerTablePersonId.push({
-      personId: parseInt(this.cells[2].innerText)
-    })
+    // registerTablePersonId.push({
+    //   personId: parseInt(this.cells[2].innerText),
+    // });
 
-    
-    registerTableData.sort( (a, b) => b.ratePoints - a.ratePoints );
-    console.table(registerTablePersonId);
+    registerTableData.sort((a, b) => b.ratePoints - a.ratePoints);
+    // console.table(registerTablePersonId);
     console.table(registerTableData);
-    
-    // registered player count
-    const registeredPlayerCount = document.getElementById(
-      'registeredPlayerCount'
-    );
-    registeredPlayerCount.innerHTML = registerTableData.length;
-    registerTableBody.innerHTML = '';
 
-    for (let i = 0; i < registerTableData.length; i++) {
-      registerTableBody.innerHTML += `
-        <tr id="reg${registerTableData[i].personId}" class="">
-          <td class="text-center">${i + 1}</td>
-          <td class="text-center">${registerTableData[i].rateOrder}</td>
-          <td class="text-center">${registerTableData[i].ratePoints}</td>
-          <td class="text-center">${registerTableData[i].personId}</td>
-          <td>${registerTableData[i].firstLastName}</td>
-          <td class="text-center">${registerTableData[i].birthdate}</td>
-          <td class="text-center">${registerTableData[i].sex}</td>
-          <td class="text-center">
-            <a class="text-danger" onclick="deleteRow(this)">
-              <i class="fas fa-trash-alt"></i>
-            </a>
-          </td>
-        </tr>
-      `;
-    }
+    reloadRows()
+    // deleteRow()
 
   });
 }
 
 
+// function deleteRow(btn) {
+//   let id = btn.parentNode.parentNode.id;
+//   console.log(id);
+//   let row = btn.parentNode.parentNode;
+//   console.log(row);
+//   // row.parentNode.removeChild(row);
 
-function deleteRow(btn) {
-  let id = btn.parentNode.parentNode.id;
-  console.log(id);
-  let row = btn.parentNode.parentNode;
-  console.log(row);
-  // row.parentNode.removeChild(row);
-
-}
+// }
 
 // function deleteRow(id) {
 //   console.log(id)
